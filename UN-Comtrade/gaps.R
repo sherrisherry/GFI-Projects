5b3801d7a8d91617fc2970e1d8657e407b94cf87
sherrisherry <34172217+sherrisherry@users.noreply.github.com>
2019-04-16 17:06:10 -0400

    distr gaps

--- UN-Comtrade/gaps.R
+++ UN-Comtrade/gaps.R
@@ -1,5 +1,5 @@
 rm(list=ls()) # clean up environment
-pkgs <- c('aws.s3', 'aws.ec2metadata', 'scripting', 'cleandata', 'remotes')
+pkgs <- c('aws.s3', 'sparklyr', 'scripting', 'remotes')
 for(i in pkgs)library(i, character.only = T)
 install_github("sherrisherry/GFI-Cloud", subdir="pkg"); library(pkg)
 
@@ -7,81 +7,122 @@ install_github("sherrisherry/GFI-Cloud", subdir="pkg"); library(pkg)
 
 usr <- 'aws00' # the user account for using AWS service
 years <- 2016:2001 # the years we want to download
-yrs_model <- 2016:2001 # the years for cifob_model
-in_dir <- '/efs/unct'
 out_bucket <- 'gfi-work' # save the results to this S3 bucket
-sup_bucket <- 'gfi-supplemental' # supplemental files
 oplog <- 'gaps.log' # progress report file
-max_try <- 10 # the maximum number of attempts for a failed process
 keycache <- read.csv('~/vars/accesscodes.csv', header = TRUE, stringsAsFactors = FALSE) # the database of our credentials
-cty <- NULL # set to NULL to select all countries within GFI's consideration; or exp. c(231, 404, 800)
-cifob_model <- 'cifob_model.rds.bz2'
-tag <- "Comtrade"
-cols_in <- c('character', rep("integer",3),rep("numeric",7),rep("integer",11)) 
-names(cols_in) <- c('k','t',"i","j","v_M","v_X","q_M","q_X", 'ln_distw', 'ln_distw_squared', 'ln_uvmdn', "q_code_M","q_code_X",
-                    'd_fob', 'd_contig', 'd_conti', 'd_rta', 'd_landlocked_i', 'd_landlocked_j', 'd_dev_i', 'd_dev_j', 'd_hs_diff')
-cols_out <- c("t","j","i","k","v_M","v_X","q_M","q_X","q_code_M","d_dev_i","d_dev_j", 'v_M_fob', 'a_wt', 'gap_wtd')
-cols_model <- c('ln_distw', 'ln_distw_squared', 'ln_uvmdn', 'd_contig', 'd_conti', 'd_rta', 'd_landlocked_i', 'd_landlocked_j', 'd_dev_i', 'd_dev_j', 'd_hs_diff')
-cols_model <- append(cols_model, paste('d', yrs_model[-1], sep = '_'))
+nload <- 3 # years for a worker
+max_try <- 10L # the maximum number of attempts for a failed process
+spark_home <- '/home/gfi/spark/spark-2.3.2-bin-hadoop2.7' # SPARK_HOME isn't required with 'local' master
+master_node <- 'spark://ip-172-31-91-141.ec2.internal:7077'
+cols_pdict <- c(rep('integer',5), 'character')
+names(cols_pdict) <- c('t', 'i', 'j', 'd_dev_i', 'd_dev_j', 'k')
 
 #===================================================================================================================================#
 				  
-oplog <- file.path('logs', oplog)
+oplog <- paste('/efs/logs', oplog, sep = '/')
 logg <- function(x)mklog(x, path = oplog)
 ec2env(keycache,usr)
-options(stringsAsFactors= FALSE)
+npar <- ceiling(n_d/nload)
 cat('Time\tZone\tYear\tMark\tStatus\n', file = oplog, append = FALSE)
+conf <- spark_config()
+conf$sparklyr.apply.env.AWS_ACCESS_KEY_ID <- Sys.getenv('AWS_ACCESS_KEY_ID')
+conf$sparklyr.apply.env.AWS_SECRET_ACCESS_KEY <- Sys.getenv('AWS_SECRET_ACCESS_KEY')
+conf$sparklyr.apply.env.AWS_DEFAULT_REGION <- Sys.getenv('AWS_DEFAULT_REGION')
+conf$sparklyr.apply.env.TMPDIR <- '/home/gfi/temp'
+conf$sparklyr.apply.env.max_try <- as.character(max_try)
+conf$spark.executor.memory <- "11GB" # the memory to request from each executor, workers having less memory are not used
+conf$spark.executor.instances <- npar
+conf$spark.dynamicAllocation.enabled <- "false"
 
-ecycle(save_object(object = cifob_model, bucket = out_bucket, file = 'tmp/tmp.csv.bz2', overwrite = TRUE),
-       {logg(paste('0000','!', 'retrieving file failed', sep = '\t')); stop()}, max_try)
-ecycle(cifob_model <- readRDS(pipe("bzip2 -dkc ./tmp/tmp.csv.bz2")),
-       {logg(paste('0000','!', 'loading file failed', sep = '\t')); stop()}, max_try)
-logg(paste('0000',':', 'loaded model', sep = '\t'))
-unlink('tmp/tmp.csv.bz2')
+sc <- spark_connect(master= master_node, config = conf)
 
-if(is.null(cty))cty <- gfi_cty(logf = logg)
-
-predicts <- list()
-for(year in years){
-  ecycle(tinv <- read.csv(bzfile(file.path(in_dir, paste(tag, year,"input.csv.bz2",sep="-"))), colClasses=cols_in, na.strings="", header = T),
-         {logg(paste(year, '!', 'loading file failed', sep = '\t')); next}, max_try)
-  logg(paste(year, ':', 'loaded data', sep = '\t'))
+dist_codes <- function(years, cols){
+  pkgs <- c('aws.s3', 'stats', 'cleandata', 'scripting', 'pkg')
+  for(i in pkgs){# spark starts a plain R session so settings taken care by RStudio need to be addressed.
+    if(!require(i, character.only = T))install.packages(i, repos = "http://cran.us.r-project.org")
+    library(i, character.only = T)}
+  #==============================================================================================#
+  
+  cty <- NULL # set to NULL to select all countries within GFI's consideration; or exp. c(231, 404, 800)
+  cifob_model <- 'cifob_model.rds.bz2'
+  tag <- 'Comtrade'
+  yrs_model <- 2016:2001 # the years for cifob_model
+  in_dir <- '/efs/unct'
+  cols_in <- c('character', rep("integer",3),rep("numeric",7),rep("integer",11)) 
+  names(cols_in) <- c('k','t',"i","j","v_M","v_X","q_M","q_X", 'ln_distw', 'ln_distw_squared', 'ln_uvmdn', "q_code_M","q_code_X",
+                      'd_fob', 'd_contig', 'd_conti', 'd_rta', 'd_landlocked_i', 'd_landlocked_j', 'd_dev_i', 'd_dev_j', 'd_hs_diff')
+  cols_out <- c("t","j","i","k","v_M","v_X","q_M","q_X","q_code_M","d_dev_i","d_dev_j", 'v_M_fob', 'a_wt', 'gap_wtd')
+  cols_model <- c('ln_distw', 'ln_distw_squared', 'ln_uvmdn', 'd_contig', 'd_conti', 'd_rta', 'd_landlocked_i', 'd_landlocked_j', 'd_dev_i', 'd_dev_j', 'd_hs_diff')
+  cols_model <- append(cols_model, paste('d', yrs_model[-1], sep = '_'))
+  
+  #==============================================================================================#
+  max_try <- as.integer(Sys.getenv('max_try'))
+  out_bucket <- Sys.getenv('out_bucket')
+  oplog <- Sys.getenv('oplog')
+  logg <- function(x)mklog(x, path = oplog)
+  options(stringsAsFactors= FALSE)
+  years <- years[,1]; cols <- cols[,1]
+  
+  ecycle(save_object(object = cifob_model, bucket = out_bucket, file = 'tmp/tmp.csv.bz2', overwrite = TRUE),
+         {logg(paste('0000','!', 'retrieving file failed', sep = '\t')); stop()}, max_try)
+  ecycle(cifob_model <- readRDS(pipe("bzip2 -dkc ./tmp/tmp.csv.bz2")),
+         {logg(paste('0000','!', 'loading file failed', sep = '\t')); stop()}, max_try)
+  logg(paste('0000',':', 'loaded model', sep = '\t'))
   unlink('tmp/tmp.csv.bz2')
-  tinv <- subset(tinv, tinv$i %in% cty | tinv$j %in% cty) # subset cty
-  logg(paste(year, ':', 'sub_cty', sep = '\t'))
-  logg(paste(year, '#', nrow(tinv), sep = '\t'))
-  tinv$d <- factor(tinv$t, levels = yrs_model); tmp <- encode_onehot(tinv[,'d', drop=FALSE], drop1st = T)
-  tinv$d <- NULL; tinv <- cbind(tinv, tmp)
-  logg(paste(year, ':', 'prepared data', sep = '\t'))
-  tinv$v_M_fob <- 0 
-  tinv[tinv$d_fob == 1,'v_M_fob'] <- tinv[tinv$d_fob == 1,'v_M']
-  pred <- predict(cifob_model, tinv[tinv$d_fob == 0, cols_model],type='response')
-  logg(paste(year, ':', 'applied model', sep = '\t'))
-  pred <- exp(pred)
-  tinv[(tinv$d_fob == 0),"v_M_fob"] <- tinv[(tinv$d_fob == 0),"v_M"] / pred 
-  tinv[(tinv$v_M_fob>tinv$v_M),"v_M_fob"] <- tinv[(tinv$v_M_fob>tinv$v_M),"v_M"]# DEFAULT: when fob>cif set FOB = CIF
-  # calculate weights & gaps
-  tinv$a_wt <- 1
-  tinv[(tinv$q_M < tinv$q_X),"a_wt"] <- 1. - ((tinv[(tinv$q_M < tinv$q_X),"q_X"]- tinv[(tinv$q_M < tinv$q_X),"q_M"])/tinv[(tinv$q_M < tinv$q_X),"q_X"])
-  tinv[(tinv$q_M >= tinv$q_X),"a_wt"] <- 1. - ((tinv[(tinv$q_M >= tinv$q_X),"q_M"]- tinv[(tinv$q_M >= tinv$q_X),"q_X"])/tinv[(tinv$q_M >= tinv$q_X),"q_M"])
-  tinv$gap <- tinv$v_M_fob - tinv$v_X
-  tinv$gap_wtd <- tinv$gap * tinv$a_wt
-  logg(paste(year, ':', 'weighted gaps', sep = '\t'))
-  file_out <- paste('tmp/', paste(tag, year, 'gaps.csv.bz2', sep = '-'), sep = '')
-  ecycle(write.csv(tinv[, cols_out], file = bzfile(file_out),row.names=FALSE,na=""), 
-         ecycle(s3write_using(tinv[, cols_out], FUN = function(x, y)write.csv(x, file=bzfile(y), row.names = FALSE),
-                              bucket = out_bucket, object = basename(file_out)),
-                logg(paste(year, '!', paste('uploading', basename(file_out), 'failed', sep = ' '), sep = '\t')), max_try), 
-         max_try,
-         ecycle(put_object(file_out, basename(file_out), bucket = out_bucket), 
-                logg(paste(year, '!', paste('uploading', basename(file_out), 'failed', sep = ' '), sep = '\t')),
-                max_try,
-                {logg(paste(year, '|', paste('uploaded', basename(file_out), sep = ' '), sep = '\t')); unlink(file_out)}))
-  predicts[[year]] <- pred
-  rm(tinv)
+  
+  if(is.null(cty))cty <- gfi_cty(logf = logg)
+  
+  predicts <- list()
+  for(year in years){
+    ecycle(tinv <- read.csv(bzfile(file.path(in_dir, paste(tag, year,"input.csv.bz2",sep="-"))), colClasses=cols_in, na.strings="", header = T),
+           {logg(paste(year, '!', 'loading file failed', sep = '\t')); next}, max_try)
+    logg(paste(year, ':', 'loaded data', sep = '\t'))
+    unlink('tmp/tmp.csv.bz2')
+    tinv <- subset(tinv, tinv$i %in% cty | tinv$j %in% cty) # subset cty
+    logg(paste(year, ':', 'sub_cty', sep = '\t'))
+    logg(paste(year, '#', nrow(tinv), sep = '\t'))
+    tinv$d <- factor(tinv$t, levels = yrs_model); tmp <- encode_onehot(tinv[,'d', drop=FALSE], drop1st = T)
+    tinv$d <- NULL; tinv <- cbind(tinv, tmp)
+    logg(paste(year, ':', 'prepared data', sep = '\t'))
+    tinv$v_M_fob <- 0; tinv <- split(tinv, tinv$'d_fob')
+    tinv$'1'$'v_M_fob' <- tinv$'1'$'v_M'
+    pdict <- predict(cifob_model, tinv$'0'[, cols_model],type='response')
+    logg(paste(year, ':', 'applied model', sep = '\t'))
+    pdict <- exp(pdict)
+    tinv$'0'$"v_M_fob" <- tinv$'0'$"v_M" / pdict
+    pdict <- cbind(tinv[, cols], pdict); tinv <- do.call(rbind, tinv)
+    tinv[(tinv$v_M_fob>tinv$v_M),"v_M_fob"] <- tinv[(tinv$v_M_fob>tinv$v_M),"v_M"]# DEFAULT: when fob>cif set FOB = CIF
+    # calculate weights & gaps
+    tinv$a_wt <- 1
+    tinv[(tinv$q_M < tinv$q_X),"a_wt"] <- 1. - ((tinv[(tinv$q_M < tinv$q_X),"q_X"]- tinv[(tinv$q_M < tinv$q_X),"q_M"])/tinv[(tinv$q_M < tinv$q_X),"q_X"])
+    tinv[(tinv$q_M >= tinv$q_X),"a_wt"] <- 1. - ((tinv[(tinv$q_M >= tinv$q_X),"q_M"]- tinv[(tinv$q_M >= tinv$q_X),"q_X"])/tinv[(tinv$q_M >= tinv$q_X),"q_M"])
+    tinv$gap <- tinv$v_M_fob - tinv$v_X
+    tinv$gap_wtd <- tinv$gap * tinv$a_wt
+    logg(paste(year, ':', 'weighted gaps', sep = '\t'))
+    file_out <- paste('tmp/', paste(tag, year, 'gaps.csv.bz2', sep = '-'), sep = '')
+    ecycle(write.csv(tinv[, cols_out], file = bzfile(file_out),row.names=F,na=""), 
+           ecycle(s3write_using(tinv[, cols_out], FUN = function(x, y)write.csv(x, file=bzfile(y), row.names = FALSE),
+                                bucket = out_bucket, object = basename(file_out)),
+                  logg(paste(year, '!', paste('uploading', basename(file_out), 'failed', sep = ' '), sep = '\t')), max_try), 
+           max_try,
+           ecycle(put_object(file_out, basename(file_out), bucket = out_bucket), 
+                  logg(paste(year, '!', paste('uploading', basename(file_out), 'failed', sep = ' '), sep = '\t')),
+                  max_try,
+                  {logg(paste(year, '|', paste('uploaded', basename(file_out), sep = ' '), sep = '\t')); unlink(file_out)}))
+    predicts[[as.character(year)]] <- pdict
+    rm(tinv)
+  }
+  rm(cifob_model)
+  return(do.call(rbind, predicts))
 }
-rm(cifob_model)
-predicts <- unlist(predicts)
-capture.output(summary(predicts), file= "data/Stats_Fitted_Exp_Values_Full.txt")
+
+logg(paste('0000', '|', 'cluster started', sep = '\t'))
+tbl_yrs <- sdf_copy_to(sc, data.frame(years), repartition = npar)
+predicts <- spark_apply(tbl_yrs, dist_codes, packages = c('pkg', 'scripting'), 
+                            context = data.frame(cols_pdict), memory = T, name = 'pred_gaps',
+                            rdd = T, columns = append(cols_pdict, c(prd = 'numeric')), 
+                            env = list(out_bucket = out_bucket, oplog = oplog))
+logg(paste('0000', '|', 'cluster ended', sep = '\t'))
+capture.output(sdf_describe(predicts), file= "data/Stats_Fitted_Exp_Values_Full.txt")
 logg(paste('0000', '|', 'summarized predictions', sep = '\t'))
 put_object(oplog, basename(oplog), bucket = out_bucket)
